4f6c601c72dcdd9c17ed3bd265bf68f66df9d508
alaattinerby <erbay3871@gmail.com>
2021-02-06 23:53:35 +0300

    css update

--- src/Components/UserCard.js
+++ src/Components/UserCard.js
@@ -6,10 +6,20 @@ function UserCard() {
     const [nameState, setNameState] = useState()
 
     return(
-        <>
-            <InputCard onChange={value => setNameState(value)} />
-            <h1>Başlık Gelecek : {nameState}</h1>
-            
+        <> 
+        <InputCard onChange={value => setNameState(value)} />
+            <section className="sectionUserCard">
+                <div className="userCard">
+                    <span>”</span>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in erat eget neque eleifend tincidunt in vel magna. 
+                        Suspendisse potenti. Aliquam erat volutpat. Aliquam hendrerit molestie feugiat. Praesent sed purus dui. 
+                        Nam consequat lacus eget laoreet tempus.
+                    </p>
+                    <img src=""></img>
+                    <h1>Başlık Gelecek : {nameState}</h1>  
+                </div>
+            </section>
         </>
     )
 }
